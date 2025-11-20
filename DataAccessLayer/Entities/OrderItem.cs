70c53e476f99dadaf1d0d041ebca2d20f20e5425
QualMinds <QualMinds@DESKTOP-FRQ690F>
2025-11-20 14:29:59 +0530

    Orders Repositories, Services

---
+++ DataAccessLayer/Entities/OrderItem.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eCommerce.OrdersMicroservice.DataAccessLayer.Entities;
+public class OrderItem
+{
+    [BsonId]
+    [BsonRepresentation(MongoDB.Bson.BsonType.String)]
+    public Guid _id { get; set; }
+
+    [BsonRepresentation(MongoDB.Bson.BsonType.String)]
+    public Guid ProductID { get; set; }
+
+    [BsonRepresentation(MongoDB.Bson.BsonType.Double)]
+    public decimal UnitPrice { get; set; }
+
+    [BsonRepresentation(MongoDB.Bson.BsonType.Int32)]
+    public int Quantity { get; set; }
+
+    [BsonRepresentation(MongoDB.Bson.BsonType.Double)]
+    public decimal TotalPrice { get; set; }
+}
---
+++ DataAccessLayer/RepositoryContracts/IOrdersRepository.cs
@@ -0,0 +1,17 @@
+﻿using eCommerce.OrdersMicroservice.DataAccessLayer.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eCommerce.OrdersMicroservice.DataAccessLayer.RepositoryContracts;
+
+public interface IOrdersRepository
+{
+    Task<IEnumerable<Order>> GetOrders();
+    Task<IEnumerable<Order?>> GetOrdersByCondition(FilterDefinition<Order> filter);
+    Task<Order?> GetOrderByCondition(FilterDefinition<Order> filter);
+    Task<Order?> AddOrder(Order order);
+    Task<Order?> UpdateOrder(Order order);
+    Task<bool> DeleteOrder(Guid orderID);
+}
